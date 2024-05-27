bd44d8c3814ee359d2f2f9435550d7a1032619a8
jongminwws <wkdwhd43@naver.com>
2024-05-27 09:39:38 +0900

    기본

--- python/start.py
+++ python/start.py
@@ -1,3 +1,13 @@
 # 화면에 hello wordl 출력
 print("hello world");
 print('hello world');
+
+
+# 화면에 hello's world 출력
+
+print("hello's wordl"); # 작은 따음표 사용할 경우 큰 따움표로 묶고 사용하면 됨
+
+
+# 두줄 입력
+print("안녕하세여. \n 만나서 \t \t 반갑습니다");
+
